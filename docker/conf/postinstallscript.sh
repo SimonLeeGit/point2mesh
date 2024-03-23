38e17c439925041286b983c7986f1257c205218b
SimonLee <lixiaomin_235@163.com>
2024-03-23 20:36:13 +0800

    add docker support

---
+++ docker/conf/postinstallscript.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# this script will run when build docker image.
+
+pip install git+https://github.com/facebookresearch/pytorch3d.git
---
+++ docker/conf/setup_env.sh
@@ -0,0 +1,9 @@
+# Docker tag for new build image
+TAG=point2mesh:dev
+CI_TAG=point2mesh:ci
+
+# Base docker image tag used by docker build
+BASE_IMG=nvcr.io/nvidia/cuda:12.3.2-runtime-ubuntu22.04
+
+# User password used in docker container
+USER_PASSWD=666666
